5f47d499962bd6d1a68219b3a6db2b54ffb25f10
JingleManSweep <jinglemansweep@gmail.com>
2010-01-18 21:08:01 +0000

    Updated Readme

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import os
 from setuptools import setup, find_packages
 setup(
     name = "lcdproc",
-    version = "0.01",
+    version = "0.02",
     author = "JingleManSweep",
     author_email = "jinglemansweep@gmail.com",
     description = "Python OOP Wrapper Library for LCDproc Telnet API",
-    url = "http://github.com/jingleman/python-lcdproc",
+    url = "http://github.com/jingleman/lcdproc",
     packages = find_packages(),
     scripts = [],
     include_package_data = True
